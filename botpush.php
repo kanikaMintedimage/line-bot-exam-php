37fa66e4dcfa12c8319993b342c09c9e70796d04
kannikaklankhiew <kannika.klankhiew@gmail.com>
2019-07-10 16:47:41 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = 'EjdWxsII5M3DyrD4b3GJdzV4dA56LPZeeEiVHSE6ACqbZ6fcb67N8z7Gwm53Rj4
 
 $channelSecret = 'c1e80ef93d097e051dfbe0c2a8b594d8';
 
-$pushID = 'U8ee83ee6ffa0bb8a31d593b75608c6a7';
+$pushID = 'U688bcbf4e8e2f0b1308a8027dcdbe400';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
